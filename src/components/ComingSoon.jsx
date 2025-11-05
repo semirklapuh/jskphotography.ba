184691afe41f90545e7b592cdd0dced2113165ce
Semir Klapuh <sema@Semirs-MacBook-Air.local>
2025-11-05 13:18:25 +0100

    build publish

--- src/components/ComingSoon.jsx
+++ src/components/ComingSoon.jsx
@@ -4,7 +4,7 @@ export default function ComingSoon() {
   return (
     <section className="h-screen bg-gradient-to-r from-pink-200 via-purple-200 to-indigo-200 flex flex-col justify-center items-center text-center px-4">
       <h1 className="text-5xl md:text-7xl font-extrabold text-gray-900 mb-6 drop-shadow-lg">
-        JSKPhotography.ba
+        JSKphotography.ba
       </h1>
       <p className="text-xl md:text-2xl text-gray-800 tracking-wide">
         is <span className="font-bold text-gray-900">coming soon</span>
