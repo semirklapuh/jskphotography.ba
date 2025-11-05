9640683f9072da71ccd723b23a010563e67f8167
Semir Klapuh <sema@Semirs-MacBook-Air.local>
2025-11-05 14:20:02 +0100

    make text good on a mobile screen

--- src/components/ComingSoon.jsx
+++ src/components/ComingSoon.jsx
@@ -3,10 +3,10 @@ import React from "react";
 export default function ComingSoon() {
   return (
     <section className="h-screen bg-gradient-to-r from-pink-200 via-purple-200 to-indigo-200 flex flex-col justify-center items-center text-center px-4">
-      <h1 className="text-5xl md:text-7xl font-extrabold text-gray-900 mb-6 drop-shadow-lg">
+      <h1 className="text-3xl md:text-7xl font-extrabold text-gray-900 mb-6 drop-shadow-lg max-w-full">
         JSKphotography.ba
       </h1>
-      <p className="text-xl md:text-2xl text-gray-800 tracking-wide">
+      <p className="text-base md:text-2xl text-gray-800 tracking-wide max-w-lg">
         is <span className="font-bold text-gray-900">coming soon</span>
       </p>
     </section>
